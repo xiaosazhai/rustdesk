f9a8fff6d6092fbba0f59ac7d139d7c4d45a3f9a
xiaosazhai <xiaosazhai@gmail.com>
2024-05-26 14:13:56 +0800

    Update en.rs

--- src/lang/en.rs
+++ src/lang/en.rs
@@ -1,7 +1,7 @@
 lazy_static::lazy_static! {
 pub static ref T: std::collections::HashMap<&'static str, &'static str> =
     [
-        ("desk_tip", "Your yingke desktop can be accessed with this ID and password."),
+        ("desk_tip", "Your yingke desktop can be accessed with this ID and password,only allow run in aibao device"),
         ("connecting_status", "Connecting to the RustDesk network..."),
         ("not_ready_status", "Not ready. Please check your connection"),
         ("ID/Relay Server", "ID/Relay server"),
